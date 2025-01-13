2b83ff082c2a0dd324402bae593446414a7fa826
mahnaz mousavi <mahnazmousavi7@gmail.com>
2025-01-13 18:21:52 +0330

    add MyApp

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using MyApp;
+using System.Drawing;
+
+
+
+//User user_01=new User();
+//user_01.FirstName = "Ali";
+//user_01.LastName = "Rezaei";
+//user_01.UserId = 1;
+//user_01.Role = RoleTye.Student;
+
+//User user_02 = new User();
+//user_02.FirstName = "Iman";
+//user_02.LastName = "Madaeny";
+//user_02.UserId = 2;
+//user_02.Role = RoleTye.TeacherAssistant;
+
+
+//User user_03 = new User();
+//user_03.FirstName = "Hamid";
+//user_03.LastName = "Reza";
+//user_03.UserId = 3;
+//user_03.Role = RoleTye.Admin;
